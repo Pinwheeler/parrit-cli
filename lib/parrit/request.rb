4a1cff7f1db5483b1f4fa3702727ae46fb2adb64
Anthony Dreessen <anthonydreessen@gmail.com>
2016-08-14 10:33:38 -0700

    Use attr_reader for syntactic sugar

--- lib/parrit/request.rb
+++ lib/parrit/request.rb
@@ -1,21 +1,12 @@
 module Parrit
-  class Request    
+  class Request
+        
+    attr_reader :verb, :route, :body
+    
     def initialize(verb, route, body)
       @verb = verb
       @route = route
       @body = body
-    end
-    
-    def verb
-      @verb
-    end
-    
-    def route
-      @route
-    end
-    
-    def body
-      @body
     end
   end
 end
